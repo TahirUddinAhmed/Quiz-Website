effb5ae2766eb40094edf8764080649cc8b3b6cf
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-02-12 10:41:04 +0530

    admin dashboard design

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <!-- Custom css file -->
     <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Online Quiz System</title>
 </head>
 <body>
@@ -35,7 +36,7 @@
       </form> -->
       <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                       <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle me-2 border rounded-pill" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> User </a>
+                          <a class="nav-link dropdown-toggle me-2" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-person"></i> User </a>
                           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                             
                               <li><a class="dropdown-item" href="profile.php">profile</a></li>
